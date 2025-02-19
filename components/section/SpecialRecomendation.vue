1f87b33a892dd4bc91e1a4ef11395328def4380c
NaturaAdnyana <natura538@gmail.com>
2025-02-19 20:42:34 +0800

    add empty state to homepage

--- components/section/SpecialRecomendation.vue
+++ components/section/SpecialRecomendation.vue
@@ -19,26 +19,44 @@
       <StoryCard :isLoading="true" />
       <StoryCard :isLoading="true" />
     </div>
-    <div
-      v-else-if="status == 'success'"
-      class="mt-6 grid grid-cols-1 lg:grid-cols-3 gap-6 mx-6 lg:mx-[110px]"
-    >
-      <StoryCard
-        class="lg:col-span-2 lg:row-span-2"
-        :data="data?.data?.stories?.data[0]"
-        :userId="user?.id || 0"
-        showAction="bookmark-only"
-      />
-      <StoryCard
-        :data="data?.data?.stories?.data[1]"
-        :userId="user?.id || 0"
-        showAction="bookmark-only"
-      />
-      <StoryCard
-        :data="data?.data?.stories?.data[2]"
-        :userId="user?.id || 0"
-        showAction="bookmark-only"
-      />
+    <div v-else-if="status == 'success'">
+      <template v-if="data?.data?.stories?.data.length === 0">
+        <div class="py-10">
+          <div class="flex flex-col items-center gap-6 text-center">
+            <h3 class="font-playfair-display font-semibold">
+              No Stories Found
+            </h3>
+            <p class="px-10">
+              There are no stories found with the current filter.
+            </p>
+          </div>
+        </div>
+      </template>
+      <template v-else>
+        <div
+          class="mt-6 grid grid-cols-1 lg:grid-cols-3 gap-6 mx-6 lg:mx-[110px]"
+        >
+          <StoryCard
+            v-show="data?.data?.stories?.data[0]"
+            class="lg:col-span-2 lg:row-span-2"
+            :data="data?.data?.stories?.data[0]"
+            :userId="user?.id || 0"
+            showAction="bookmark-only"
+          />
+          <StoryCard
+            v-show="data?.data?.stories?.data[1]"
+            :data="data?.data?.stories?.data[1]"
+            :userId="user?.id || 0"
+            showAction="bookmark-only"
+          />
+          <StoryCard
+            v-show="data?.data?.stories?.data[2]"
+            :data="data?.data?.stories?.data[2]"
+            :userId="user?.id || 0"
+            showAction="bookmark-only"
+          />
+        </div>
+      </template>
     </div>
   </section>
 </template>
